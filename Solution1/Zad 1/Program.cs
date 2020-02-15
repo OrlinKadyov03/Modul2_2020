fac435685df66b69eb8678de3d05a0c82318904e
orlin <orlin221@abv.bg>
2020-02-15 12:30:35 +0200

    fsadf

---
+++ Solution1/Zad 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int centery = int.Parse(Console.ReadLine());
+            int years = centery * 100;
+            int days = (int)(years * 365.2422);
+            int hours = 24 * days;
+            int minutes = 60 * hours;
+            Console.WriteLine("{0}centuries = {1} years = {2} days = {3} hours = {4} minutes", centery, years, days, hours, minutes);
+            Console.Write("Centery = ");
+
+        }
+    }
+}
