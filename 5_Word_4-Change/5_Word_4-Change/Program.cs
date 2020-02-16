e841e2c37447a7149e53d30fd3615e1184ac539b
orlin <orlin221@abv.bg>
2020-02-16 10:03:43 +0200

    ьяа

---
+++ 5_Word_4-Change/5_Word_4-Change/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Word_4_Change
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = a;
+            Console.WriteLine("before");
+            Console.WriteLine($"a={a}");
+            Console.WriteLine($"b={b}");
+            a = b;
+            b = c;
+            c = a;
+            Console.WriteLine("after");
+            Console.WriteLine($"a={a}");
+            Console.WriteLine($"b={b}");
+
+
+        }
+    }
+}
