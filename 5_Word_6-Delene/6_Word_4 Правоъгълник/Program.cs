d01303ef8b695a53bd63999595912ed6edafa70a
orlin <orlin221@abv.bg>
2020-02-16 11:41:28 +0200

    Pravo

---
+++ 5_Word_6-Delene/6_Word_4 Правоъгълник/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_Word_4_Правоъгълник
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+           double b = double.Parse(Console.ReadLine());
+
+
+            double s = a * b;
+            double p = 2 * a + 2 * b;
+            double c = Math.Sqrt(Math.Pow(a, 2) + b * b);
+            decimal c1 =(decimal) Math.Sqrt(Math.Pow(a,2) + b * b);
+            
+
+            Console.WriteLine(s);
+            Console.WriteLine(p);
+            Console.WriteLine(c);
+            Console.WriteLine(c1);
+
+        }
+    }
+}
