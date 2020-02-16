c6fedececea5823b278546f8570e8d497eb51953
orlin <orlin221@abv.bg>
2020-02-16 11:12:32 +0200

    Delene maina

---
+++ 5_Word_6-Delene/5_Word_6-Delene/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Word_6_Delene
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+                sum += a % b;
+            }
+            
+            Console.WriteLine($"Sum={sum}");
+
+
+        }
+    }
+}
