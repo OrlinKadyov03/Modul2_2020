9fd3a8dc9de87632736bbe412916ae7086db67c5
orlin <orlin221@abv.bg>
2020-02-22 11:48:56 +0200

    оаяо

---
+++ Модул2_Масиви/10_Word_1SumMaxMid/10_Word_1SumMaxMid/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Word_1SumMaxMid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Console.WriteLine(string.Join(" ",num));
+            int sumEl = num.Sum();
+            Console.WriteLine($" sum={sumEl}");
+
+            int maxEl = num.Max();
+            Console.WriteLine($" max={maxEl}");
+
+            int minEl = num.Min();
+            Console.WriteLine($" min={minEl}");
+            double aveEl = num.Average();
+            Console.WriteLine($" ave={aveEl:f2}");
+
+        }
+    }
+}
