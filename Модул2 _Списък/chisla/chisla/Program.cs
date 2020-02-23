71472e0ea181c8d8089b33f17fe6a736db658bed
orlin <orlin221@abv.bg>
2020-02-23 12:34:42 +0200

    ояояа

--- Модул2 _Списък/chisla/chisla/Program.cs
+++ Модул2 _Списък/chisla/chisla/Program.cs
@@ -4,17 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Числа
+namespace chisla
 {
     class Program
     {
         static void Main(string[] args)
         {
-            List<int> num = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
-            foreach (var item in num)
-            {
-                Console.WriteLine(item);
-            }
         }
     }
 }
--- Модул2 _Списък/chisla/chisla/Properties/AssemblyInfo.cs
+++ Модул2 _Списък/chisla/chisla/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Числа")]
+[assembly: AssemblyTitle("chisla")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Числа")]
+[assembly: AssemblyProduct("chisla")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("22a93120-b9d5-4673-8fb3-824958c54fb5")]
+[assembly: Guid("cb26531f-7647-48dc-be46-95fcf7ea3739")]
 
 // Version information for an assembly consists of the following four values:
 //
