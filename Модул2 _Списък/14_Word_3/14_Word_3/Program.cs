7fed9de5b506384b3414825a008248bd4516c058
orlin <orlin221@abv.bg>
2020-02-23 12:54:04 +0200

    оьяо

--- Модул2 _Списък/14_Word_3/14_Word_3/Program.cs
+++ Модул2 _Списък/14_Word_3/14_Word_3/Program.cs
@@ -4,27 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace chisla
+namespace _14_Word_3
 {
     class Program
     {
         static void Main(string[] args)
         {
             List<int> num = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
-            Console.WriteLine(string.Join(" ", num));
-            int brchetni = 0;
+
             num.Sort();
-            Console.WriteLine(string.Join(" ", num));
+            
+
+            int Max = num.Max();
+           
+
+            int Min = num.Min();
+            
+
             foreach (var item in num)
             {
-                if (item % 2 == 0)
+                if (item==Min)
                 {
-                    brchetni++;
-                    Console.WriteLine(item);
+                    Console.WriteLine(item + " ");
                 }
             }
-            Console.WriteLine($"Broy na chetni:{brchetni}");
+            foreach (var item in num)
+            {
+                if (item==Max)
+                {
+                    Console.WriteLine(item+ " ");
+                }
+            }
+         
 
+            
 
 
         }
