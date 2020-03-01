991421fdda3aa29cfcc220eb49281e0aa4bca8dc
orlin <orlin221@abv.bg>
2020-03-01 11:59:59 +0200

    Zadachi

---
+++ Модул2 Речник/2_Word_Telefonen ukazatel_27/2_Word_Telefonen ukazatel_27/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Word_Telefonen_ukazatel_27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> telef = new Dictionary<string, string>();
+            
+            while (true)
+            {
+                var linia = Console.ReadLine().Split();
+                var command = linia[0];
+
+                if ( command == "end")
+                {
+                    break;
+                }
+                
+                if (command == "add")
+                {
+                    var name = linia[1];
+                    var number = linia[2];
+                    if (telef.ContainsKey(name))
+                    {
+                        telef[name] = number;
+                    }
+                    else
+                    {
+                        telef.Add(name, number);
+                    }
+                }
+
+                if (command == "print")
+                {
+                    var name = linia[1];
+                    if (telef.ContainsKey(name))
+                    {
+
+                        Console.WriteLine($"Contact{name} ->{telef[name]} ");
+                    }
+                    else
+                    {
+
+                        Console.WriteLine($"Contact {name} does not exist ");
+                    } 
+       
+                }
+            }
+        }
+    }
+}
