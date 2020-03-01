614ba24191e428d9001cb535eecb01bb7b9c7159
orlin <orlin221@abv.bg>
2020-03-01 12:52:03 +0200

    ъюою

---
+++ Модул2 Речник/stoprechnik/stoprechnik/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stoprechnik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> person = new Dictionary<string, int>
+            {
+                {"Anyn",2112},
+                {"Anyn ",222},
+                {"to ",221},
+                {"molo ",225},
+                {"Anyn to molo ",122},
+            };
+            foreach (var item in person)
+   
+            {
+                Console.WriteLine($"{item.Key}:{item.Value}");
+            }
+            var sortperson = person.OrderBy(x => x.Key);
+            Console.WriteLine("=====");
+            foreach (var item in sortperson)
+            {
+                Console.WriteLine($"{item.Key}:{item.Value} age");
+            }
+            Console.WriteLine("===SortAgeByDes==");
+            var sortAgeDes = person.OrderByDescending(x=> x.Value);
+            foreach (var item in sortperson)
+            {
+                Console.WriteLine($"{item.Key}:{item.Value} age");
+            }
+
+
+
+        }
+    }
+}
